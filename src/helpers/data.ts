86053cc1c3f683f47813dd565e8fe5a43816792e
tao <2098613620@qq.com>
2022-09-24 21:37:20 +0800

    1

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -2,6 +2,7 @@ import { isPlainObject } from './util'
 
 export function transformRequest(data: any): any {
   if (isPlainObject(data)) {
+    console.log('JSON.stringify(data)')
     return JSON.stringify(data)
   }
   return data
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { bulidURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 
 function axios(config: AxiosRequestConfig): void {
   processConfig(config)
@@ -8,12 +10,23 @@ function axios(config: AxiosRequestConfig): void {
 }
 
 function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformUrl(config)
+  config.url = transformURL(config)
+  config.headers = transformHeaders(config)
+  config.data = transformRequestData(config)
 }
 
-function transformUrl(config: AxiosRequestConfig): string {
+function transformURL(config: AxiosRequestConfig): string {
   const { url, params } = config
   return bulidURL(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
+
+function transformHeaders(config: AxiosRequestConfig) {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 export default axios
